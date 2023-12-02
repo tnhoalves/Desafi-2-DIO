befa905487fec555e17a2a40580cf9179ff1749a
tnhoalves <tnhoalves@gmail.com>
2023-12-02 19:12:45 -0300

    resultado ranqueada

---
+++ index.js
@@ -0,0 +1,36 @@
+let classe= resultado (5000,3411)
+let winrate = classe
+ console.log("Seu win rate é de: " + classe)
+
+function resultado(vitorias,derrotas){
+   let soma = vitorias - derrotas
+   return soma
+
+}
+
+
+function ranqueada(){
+    let resultadoRank = winrate
+    let elo = ""
+    let taxa =elo
+ if (resultadoRank <= 10){
+    elo= "Ferro"
+ } else if (resultadoRank <= 20){
+    elo= "bronze"
+ } else if(resultadoRank <= 50){
+    elo= "Prata"
+ } else if(resultadoRank <= 80){
+    elo="Ouro"
+ } else if(resultadoRank <= 90){
+    elo="Diamante"
+ } else if(resultadoRank <= 100){
+    elo=" Lendário"
+ } else if(resultadoRank >= 101){
+    elo="Imortal"
+ }
+ 
+ return "O Herói tem de winrate de:" + resultadoRank + ". Por isso está no nível de " + elo
+
+}
+
+console.log(ranqueada())
